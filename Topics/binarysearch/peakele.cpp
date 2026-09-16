1712a5d2b996fe4d90719787309a5043ec2e00df
Abrar Fayaz <bhatabrar517@gmail.com>
2026-09-16 18:02:34 +0530

    Read Peak element

---
+++ Topics/binarysearch/peakele.cpp
@@ -0,0 +1,26 @@
+/*A peak element is an element that is strictly greater than its neighbors.
+Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.*/
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int high=nums.size()-1;
+        int low=0;
+        int n=nums.size();
+        if(n==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
+        low=1;
+        high=n-2;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
+            else if(nums[mid]>nums[mid-1]) low=mid+1;
+            else if(nums[mid]<nums[mid+1]) high=mid-1;
+            else high=mid-1;
+        }
+        return -1;
+
+    }
+};
